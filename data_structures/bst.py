6d6c6ef2e42a2b7a80b2eab0b195ccd90b750cd6
kpace <krasimirva@gmail.com>
2016-04-12 07:23:23 +0300

    BInary Search Tree added

---
+++ data_structures/bst.py
@@ -0,0 +1,128 @@
+class BinarySearchTree:
+    class Node:
+        def __init__(self, key, data, left=None, right=None, parent=None):
+            self.key = key
+            self.data = data
+            self.left = left
+            self.right = right
+            self.parent = parent
+
+        def child_count(self):
+            if self.left and self.right:
+                return 2
+            elif self.left or self.right:
+                return 1
+            else:
+                return 0
+
+        def is_left_child(self):
+            return self.parent and self.parent.left is self
+
+        def is_right_child(self):
+            return self.parent and not self.is_left_child()
+
+    __root = None
+
+    def add(self, key, data):
+        if self.__root is None:
+            self.__root = BinarySearchTree.Node(key, data)
+        else:
+            self._add(key, data, self.__root)
+
+    def _add(self, key, data, current):
+        if key < current.key:
+            if current.left is None:
+                current.left = BinarySearchTree.Node(key, data, parent=current)
+            else:
+                self._add(key, data, current.left)
+        else:
+            if current.right is None:
+                current.right = BinarySearchTree.Node(key, data, parent=current)
+            else:
+                self._add(key, data, current.right)
+
+    def pre_order(self):
+        def _rec(current):
+            if current.left:
+                _rec(current.left)
+
+            print('key:%s,\tvalue:%s' % (str(current.key), str(current.data)))
+
+            if current.right:
+                _rec(current.right)
+
+        if self.__root:
+            _rec(self.__root)
+
+    def get(self, key):
+        res = self._get(key, self.__root)
+        if res:
+            return res.data
+
+    def _get(self, key, current):
+        if current is None:
+            return None
+        elif key == current.key:
+            return current
+        elif key < current.key:
+            return self._get(key, current.left)
+        else:
+            return self._get(key, current.right)
+
+    def empty(self):
+        return self.__root is None
+
+    def delete(self, key):
+        to_remove = self._get(key, self.__root)
+
+        if to_remove:
+            if to_remove.child_count() == 0 or to_remove.child_count() == 1:
+                self._remove(to_remove)
+            elif to_remove.child_count() == 2:
+                right_min = self._find_min(to_remove.right)
+                to_remove.key = right_min.key
+                to_remove.data = right_min.data
+                self._remove(right_min)
+
+    def _remove(self, to_remove):
+        if to_remove.child_count() == 0:
+            if to_remove.is_left_child():
+                to_remove.parent.left = None
+            else:
+                to_remove.parent.right = None
+        elif to_remove.child_count() == 1:
+            to_remove_child = to_remove.left if to_remove.left else to_remove.right
+            if to_remove.is_left_child():
+                to_remove.parent.left = to_remove_child
+            else:
+                to_remove.parent.right = to_remove_child
+
+    def _find_min(self, current):
+        if current.left:
+            return self._find_min(current.left)
+        else:
+            return current
+
+    def __contains__(self, key):
+        return self._get(key, self.__root)
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+t = BinarySearchTree()
+
+t.add(4, 'a')
+t.add(15, 'b')
+t.add(32, 'c')
+t.add(6, 'd')
+t.add(13, 'e')
+t.add(48, 'f')
+t.add(2, 'g')
+
+t.delete(15)
+t.delete(2)
+assert 15 not in t
+assert 2 not in t
+
+
+t.pre_order()
